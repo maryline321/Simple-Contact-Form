ed51b45f5b81e30db21deb766aae125af4b956b2
maryline321 <marylinechebet2@gmail.com>
2021-04-26 14:27:24 -0700

    first commit

---
+++ MapsExample/app/src/main/java/com/example/mapsexample/google_maps.kt
@@ -0,0 +1,11 @@
+package com.example.mapsexample
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class google_maps : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_google_maps)
+    }
+}
